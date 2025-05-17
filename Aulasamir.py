d30519d397975cb10efa4bb4da5eb1d74ff01ac1
Luizphelipe31 <lipebusinesswork@gmail.com>
2025-05-17 10:41:48 -0300

    prática 01

---
+++ Aulasamir.py
@@ -0,0 +1,30 @@
+print ("ola mundo")
+
+numero1 = 12
+numero2 = 14
+
+soma = numero1 + numero2
+print("A soma é:", soma)
+
+
+comprimento = 12
+largura = 14
+altura = 20
+
+volume = comprimento * largura * altura
+
+print("O volume da caixa é:", volume, "cm³")
+
+
+produto = "Cadeira Infantil"
+
+preco = 12.40
+
+quantidade = 3
+
+preco_total = preco * quantidade
+
+print("Produto:", produto)
+print("Preço de cada unidade: R$", preco)
+print("Quantidade comprada:", quantidade)
+print("Preço total da compra: R$", preco_total)
